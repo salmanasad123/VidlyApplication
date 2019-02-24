df799b6bd520d8fd2ba58decbebf5977a784cb95
slmanasad <slman.me@gmail.com>
2019-02-24 16:41:13 +0500

    rentals added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies');
 
 const p = mongoose.connect('mongodb://localhost/vidly');
 p.then(function () {
@@ -16,6 +17,7 @@ app.use(express.json());
 
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('api/movies', movies);
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
